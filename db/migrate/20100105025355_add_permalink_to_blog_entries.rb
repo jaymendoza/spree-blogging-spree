6490546451b643af24e83e90e85759fdc0a4a0f6
Jay Mendoza <jayzer@gmail.com>
2010-01-05 21:13:26 -0600

    update migration to use permalink instead of url

--- db/migrate/20100105025355_add_permalink_to_blog_entries.rb
+++ db/migrate/20100105025355_add_permalink_to_blog_entries.rb
@@ -1,9 +1,9 @@
 class AddPermalinkToBlogEntries < ActiveRecord::Migration
   def self.up
-    add_column :blog_entries, :url, :string
+    add_column :blog_entries, :permalink, :string
   end
 
   def self.down
-    remove_column :blog_entries, :url
+    remove_column :blog_entries, :permalink
   end
 end
