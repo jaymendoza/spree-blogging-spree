9dc62c07e2ab070faff1e3d1f3e6c41f9c593fe6
Jay Mendoza <jayzer@gmail.com>
2010-01-17 04:00:47 -0600

    [#1] Rewrite news archive widget
    * Refactor open/close, hide/show
    * Add month/year links to respective archive pages
    * Add arrows and better color scheme
    * Fix miscellaneous year/month sorting issues

--- public/javascripts/news_archive_widget.js
+++ public/javascripts/news_archive_widget.js
@@ -1,29 +1,14 @@
 jQuery(function() {
-  $('.months li').children('ul:not(:first)').hide();
-  $('.months li').children(':first').show();
+  $('#news-archive')
+    .find('ul').hide()
+      .filter('.months:first').show().end()
+      .filter('.posts:first').show().end()
+      .end()
 
-  $('ul.months > li').click(function(evt) {
-    var entry_list = $(this).find('ul');
+    .find('span:lt(2)').addClass('open').end()
 
-    if($(evt.target).parent().hasClass('months')) {
-      if(entry_list.is(':visible')) {
-        entry_list.slideUp('fast');
-      } else {
-        entry_list.slideDown('fast');
-      }
-    }
-  });
-
-  $('li.year').click(function(evt) {
-    var month_list = $(this).find('ul.months');
-
-    if($(evt.target).parent().is('#news-archive')) {
-      if(month_list.is(':visible')) {
-        month_list.find('ul.posts').slideUp('fast');
-        month_list.slideUp('fast');
-      } else {
-        month_list.slideDown('fast');
-      }
-    }
-  });
+    .find('span').click(function() {
+      $(this).toggleClass('open');
+      $(this).siblings('ul:first').toggle();
+    });
 });
