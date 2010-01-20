4e86edc7cf9c542a31b6a6c2b17a44bdbfdedadb
krick <antipin.konstantin@googlemail.com>
2010-01-20 15:38:41 +0300

    Adopt layout to the spree-edge style. Using hooks instead of overriding layout.

--- blogging_spree_hooks.rb
+++ blogging_spree_hooks.rb
@@ -2,4 +2,9 @@ class BloggingSpreeHooks < Spree::ThemeSupport::HookListener
   insert_after :admin_tabs do
     %(<%=  tab(:blog, { :route => "admin_blog_entries" })  %>)
   end
+
+  insert_after :inside_head do
+    %(<%= javascript_include_tag 'news_archive_widget' %>
+      <%= stylesheet_link_tag 'blog' %>)
+  end
 end
