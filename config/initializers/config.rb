127a7dc928e7a9fce594f358b951fca3105789de
Sam Beam <sbeam@onsetcorps.net>
2011-03-07 08:29:17 -0500

    avoid rake complaints about Spree::Config for now
    (2 config options should be settable via backend)

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,3 +1,5 @@
-Spree::Config.set(:blog_entries_per_page => 3)  
-Spree::Config.set(:blog_entries_recent_sidebar => 6)
+if Spree::Config
+    Spree::Config.set(:blog_entries_per_page => 3)  
+    Spree::Config.set(:blog_entries_recent_sidebar => 6)
+end
 
